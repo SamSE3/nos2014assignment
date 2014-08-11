4a2bb71a8b1c39fbde3d30359a11e169558f1f7d
gardners <paul@servalproject.org>
2014-08-11 21:25:17 +0930

    fix test for 020 server message on connection.

--- test.c
+++ test.c
@@ -250,7 +250,7 @@ int test_next_response_is(int code,char *mynick,char *buffer,int *bytes)
   char serverid[*bytes];
   char thenick[*bytes];
   char themessage[*bytes];
-  int r=sscanf(buffer,":%[^ ] %d %s %[^\n]%n",
+  int r=sscanf(buffer,":%[^ ] %*[0]%d %s %[^\n]%n",
 	       serverid,&thecode,thenick,themessage,&n);
   if (n>0&&(n<=(*bytes))) {
     bcopy(&buffer[n],&buffer[0],(*bytes)-n);
@@ -324,7 +324,7 @@ int test_servergreeting()
   if (bytes>8191) bytes=8191;
   if (bytes>=0&&bytes<8192) buffer[bytes]=0;
   // Check for initial server greeting
-  test_next_response_is(020,"*",buffer,&bytes);
+  test_next_response_is(20,"*",buffer,&bytes);
   // check that there is nothing more in there
   failif(bytes>0,
 	 "Extraneous server message(s)",
