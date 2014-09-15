32b313357accd62d353849cf6732ee8f3b4a96fa
SuperNova <SuperNova@TheWillOfD>
2014-09-15 21:10:28 +0930

    rearranged code moved QUIT up, added USER and NICK cases if statements (no body yet)

--- sample.c
+++ sample.c
@@ -18,7 +18,9 @@
   along with this program; if not, write to the Free Software
   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-*/
+ */
+
+//http://www.anta.net/misc/telnet-troubleshooting/irc.shtml
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -38,165 +40,186 @@
 #include <ctype.h>
 
 struct client_thread {
-  pthread_t thread;
-  int thread_id;
-  int fd;
+    pthread_t thread;
+    int thread_id;
+    int fd;
 
-  char nickname[32];
+    char nickname[32];
 
-  int state;
-  int user_command_seen;
-  int user_has_registered;
-  time_t timeout;
+    int state;
+    int user_command_seen;
+    int user_has_registered;
+    time_t timeout;
 
-  char line[1024];
-  int line_len;
+    char line[1024];
+    int line_len;
 
-  int next_message;
+    int next_message;
 };
 
 pthread_rwlock_t message_log_lock;
 
-int read_from_socket(int sock,unsigned char *buffer,int *count,int buffer_size,
-		     int timeout)
-{
-  fcntl(sock,F_SETFL,fcntl(sock, F_GETFL, NULL)|O_NONBLOCK); // make sure sock wont block
+int read_from_socket(int sock, unsigned char *buffer, int *count, int buffer_size,
+        int timeout) {
+    fcntl(sock, F_SETFL, fcntl(sock, F_GETFL, NULL) | O_NONBLOCK); // make sure sock wont block
 
-  int t=time(0)+timeout;
-  if (*count>=buffer_size) return 0; // got some data return zero
-  int r=read(sock,&buffer[*count],buffer_size-*count);
-  //address of the 0th elem in the array buffer
-  while(r!=0) {
-    if (r>0) {
-      (*count)+=r;
-      break;
+    int t = time(0) + timeout;
+    if (*count >= buffer_size) return 0; // got some data return zero
+    int r = read(sock, &buffer[*count], buffer_size - *count);
+    //address of the 0th elem in the array buffer
+    while (r != 0) {
+        if (r > 0) {
+            (*count) += r;
+            break;
+        }
+        r = read(sock, &buffer[*count], buffer_size - *count);
+        if (r == -1 && errno != EAGAIN) { // no double error
+            perror("read() returned error. Stopping reading from socket.");
+            return -1;
+        } else usleep(100000); // sleep 
+        // timeout after a few seconds of nothing
+        if (time(0) >= t) break; //check the timeout
     }
-    r=read(sock,&buffer[*count],buffer_size-*count);
-    if (r==-1&&errno!=EAGAIN) { // no double error
-      perror("read() returned error. Stopping reading from socket.");
-      return -1;
-    } else usleep(100000); // sleep 
-    // timeout after a few seconds of nothing
-    if (time(0)>=t) break; //check the timeout
-  }
-  buffer[*count]=0; //null the end of the string
-  return 0;
+    buffer[*count] = 0; //null the end of the string
+    return 0;
 }
 
-int create_listen_socket(int port)
-{
-  int sock = socket(AF_INET,SOCK_STREAM,0);
-  if (sock==-1) return -1;
+int create_listen_socket(int port) {
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == -1) return -1;
 
-  int on=1;
-  if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, (char *)&on, sizeof(on)) == -1) {
-    close(sock); return -1;
-  }
-  if (ioctl(sock, FIONBIO, (char *)&on) == -1) {
-    close(sock); return -1;
-  }
-  
-  /* Bind it to the next port we want to try. */
-  struct sockaddr_in address;
-  bzero((char *) &address, sizeof(address));
-  address.sin_family = AF_INET;
-  address.sin_addr.s_addr = INADDR_ANY;
-  address.sin_port = htons(port);
-  if (bind(sock, (struct sockaddr *) &address, sizeof(address)) == -1) {
-    close(sock); return -1;
-  } 
+    int on = 1;
+    if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, (char *) &on, sizeof (on)) == -1) {
+        close(sock);
+        return -1;
+    }
+    if (ioctl(sock, FIONBIO, (char *) &on) == -1) {
+        close(sock);
+        return -1;
+    }
 
-  if (listen(sock, 20) != -1) return sock;
+    /* Bind it to the next port we want to try. */
+    struct sockaddr_in address;
+    bzero((char *) &address, sizeof (address));
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(port);
+    if (bind(sock, (struct sockaddr *) &address, sizeof (address)) == -1) {
+        close(sock);
+        return -1;
+    }
 
-  close(sock);
-  return -1;
+    if (listen(sock, 20) != -1) return sock;
+
+    close(sock);
+    return -1;
 }
 
-int accept_incoming(int sock)
-{
-  struct sockaddr addr;
-  unsigned int addr_len = sizeof addr;
-  int asock;
-  if ((asock = accept(sock, &addr, &addr_len)) != -1) {
-    return asock;
-  }
+int accept_incoming(int sock) {
+    struct sockaddr addr;
+    unsigned int addr_len = sizeof addr;
+    int asock;
+    if ((asock = accept(sock, &addr, &addr_len)) != -1) {
+        return asock;
+    }
 
-  return -1;
+    return -1;
 }
 
-int connection_count=0;
+int connection_count = 0;
 
-int handle_connection(int fd)
-{
-  //printf("I have now seen %d connections so far.\n",++connection_count);
-  char msg[1024];
-  snprintf(msg,1024,":myserver.com 020 * :hello\n");
-  write(fd,msg,strlen(msg));
-  unsigned char buffer[8192];
-  int length=0;
-  int registered=0;
+int handle_connection(int fd) {
+    //printf("I have now seen %d connections so far.\n",++connection_count);
+    char msg[1024];
+    snprintf(msg, 1024, ":myserver.com 020 * :hello\n");
+    write(fd, msg, strlen(msg));
+    unsigned char buffer[8192];
+    int length = 0;
+    //int registered = 0;
+    int state = 0;
+
+    /*
+     * states as defined by this program
+     * 0 unregistered
+     * 1 password received
+     * 2 nickname set
+     * 3 registered (username set)
+     */
+
+    while (1) {
+        length = 0;
+        read_from_socket(fd, buffer, &length, 8192, 5);
+        if (length == 0) {
+            snprintf(msg, 1024, "ERROR :Closing Link: Connection timed out (bye bye)\n");
+            write(fd, msg, strlen(msg));
+            close(fd);
+            return 0;
+        }
+
+        /*
+        int possComLength = (int) strchr(buffer, ' ');
+        if (possComLength != 0) {
+            possComLength += 1 - (int) &buffer[0];
+            //printf("the length is %d\n",possComLength);   
+            //printf("the command is %.*s\n", possComLength, buffer);
+        }
+         */
+
+        buffer[length] = 0;
+
+        if (strncasecmp("QUIT", (char*) buffer, 4) == 0) {
+            // client has said they are going away
+            // needed to avoid SIGPIPE and the program will be killed on socket read
+            snprintf(msg, 1024, "ERROR :Closing Link: Connection timed out (bye bye)\n");
+            write(fd, msg, strlen(msg));
+            close(fd);
+            return 0;
+        }
+
+        if (strncasecmp("JOIN", (char*) buffer, 4) == 0) {
+            if (state == 3) {
+                snprintf(msg, 1024, ":myserver.com 241 * :JOIN command sent before registration\n");
+                write(fd, msg, strlen(msg));
+            }
+            //@todo handle legit join here
+        } else if (strncasecmp("PRIVMSG", (char*) buffer, 7) == 0) {
+            if (state == 3) {
+                snprintf(msg, 1024, ":myserver.com 241 * :PRIVMSG command sent before registration\n");
+                write(fd, msg, strlen(msg));
+            }
+            //@todo handle legit private message here
+        } else if (strncasecmp("NICK", (char*) buffer, 4) == 0) {
+            
+        }  else if (strncasecmp("USER", (char*) buffer, 4) == 0) {
+            
+        }
 
 
-  while(1){
-    length=0;
-    read_from_socket(fd,buffer,&length,8192,5);
-    if(length==0){
-      snprintf(msg,1024,"ERROR :Closing Link: Connection timed out (bye bye)\n");
-      write(fd,msg,strlen(msg));
-      close(fd);
-      return 0;
+
     }
-    
-    buffer[length]=0;
-    //printf("the test program sent '%s",buffer);
-  
-    char channel[8192];
-    int  r=sscanf((char*) buffer,"JOIN %s", channel);
-    if(r==1){
-      if(!registered){
-	snprintf(msg,1024,":myserver.com 241 * :JOIN command sent before registration\n");
-	write(fd,msg,strlen(msg));
-      }
-    }
-
-    if(!strncasecmp("PRIVMSG",(char*) buffer,7)){
-      snprintf(msg,1024,":myserver.com 241 * :PRIVMSG command sent before registration\n");
-      write(fd,msg,strlen(msg));
-    }
-
-    if(!strncasecmp("QUIT",(char*) buffer,4)){
-      // client has said they are going away
-      // needed to avoid SIGPIPE and the program will be killed on socket read
-      snprintf(msg,1024,"ERROR :Closing Link: Connection timed out (bye bye)\n");
-      write(fd,msg,strlen(msg));
-      close(fd);
-      return 0;
-    }
-  }
 
 
-  close(fd);
-  return 0;
+    close(fd);
+    return 0;
 }
 
-int main(int argc,char **argv)
-{
-  signal(SIGPIPE, SIG_IGN);
+int main(int argc, char **argv) {
+    signal(SIGPIPE, SIG_IGN);
 
-  if (argc!=2) {
-  fprintf(stderr,"usage: sample <tcp port>\n");
-  exit(-1);
-  }
-  
-  int master_socket = create_listen_socket(atoi(argv[1]));
-  
-  fcntl(master_socket,F_SETFL,fcntl(master_socket, F_GETFL, NULL)&(~O_NONBLOCK));  
-  
-  while(1) {
-    int client_sock = accept_incoming(master_socket);
-    if (client_sock!=-1) {
-      // close(client_sock);
-      handle_connection(client_sock);
+    if (argc != 2) {
+        fprintf(stderr, "usage: sample <tcp port>\n");
+        exit(-1);
     }
-  }
+
+    int master_socket = create_listen_socket(atoi(argv[1]));
+
+    fcntl(master_socket, F_SETFL, fcntl(master_socket, F_GETFL, NULL)&(~O_NONBLOCK));
+
+    while (1) {
+        int client_sock = accept_incoming(master_socket);
+        if (client_sock != -1) {
+            // close(client_sock);
+            handle_connection(client_sock);
+        }
+    }
 }
